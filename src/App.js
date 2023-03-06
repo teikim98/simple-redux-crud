32d750bcdc98a16cd82090793e144c35316cc404
teikim98 <teikim98@gmail.com>
2023-03-06 11:09:30 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  const userList = useSelector((state) => state.users.value);
+  return (
+    <div className="App">
+      {""}
+      <div className="addUsers">
+        <input type="text" placeholder="Write yout name..."></input>
+        <input type="text" placeholder="Write yout username..."></input>
+        <button>Add User</button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((users) => {
+          return (
+            <div>
+              <h1>{users.name}</h1>
+              <h2>{users.username}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
